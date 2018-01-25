9ce7ecff952b919cd4665a498a0f0cbe74d6f5dc
liebersonsantos <liebersonsantos.one@gmail.com>
2018-01-25 15:57:36 -0200

    Initial commit

---
+++ app/src/main/java/com/example/lieberson/adivinha/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lieberson.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        botaoJogar = findViewById(R.id.botaoJogarId);
+        textoResultado = findViewById(R.id.textoResultadoId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+
+                textoResultado.setText("Número escolhido: " + numeroAleatorio);
+            }
+        });
+
+
+
+    }
+}
